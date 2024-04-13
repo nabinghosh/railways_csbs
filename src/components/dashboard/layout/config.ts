8dc0902c470c1abecf7eefc88c195f69acb58c98
nabinghosh <www.champaghosha.12345@gmail.com>
2024-04-14 01:10:53 +0530

    .

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -6,7 +6,7 @@ export const navItems = [
   { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
   { key: 'customers', title: 'Reservation', href: paths.dashboard.customers, icon: 'users' },
   { key: 'integrations', title: 'Schedule', href: paths.dashboard.integrations, icon: 'plugs-connected' },
-  { key: 'settings', title: 'Trains', href: paths.dashboard.settings, icon: 'gear-six' },
+  { key: 'Trains', title: 'Trainssssss', href: paths.dashboard.settings, icon: 'gear-six' },
   // { key: 'account', title: 'Add / Delete Train', href: paths.dashboard.account, icon: 'x-square' },
   // { key: 'error', title: 'Error', href: paths.errors.notFound, icon: 'x-square' },
 ] satisfies NavItemConfig[];
